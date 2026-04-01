dd522b9c60c57e05a859fcfb22ce7e199ac2ca18
Aaron <aarongreen193@hotmail.com>
2026-04-01 23:12:08 +0100

    inital commit

--- pages/account.js
+++ pages/account.js
@@ -43,7 +43,7 @@ export default function Account() {
                 )
               }
             >
-              Manage Subscription
+              Cancel Subscription
             </button>
           )}
 
